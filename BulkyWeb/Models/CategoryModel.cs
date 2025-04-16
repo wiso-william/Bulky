c5bfb39cc0f636b9ae37d21cc8c9b811594979a0
wiso-william <williammancy0@gmail.com>
2025-04-16 14:52:00 +0200

    Project initialized

---
+++ BulkyWeb/Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class CategoryModel
+    {
+        // You can add [Key] to define something as the primary key
+        // ASP infers that "Id" is the primary key
+        // CategoryModelId would aslo be automatically infered to be the id
+        public int Id { get; set; }
+        [Required] // Not null SQL
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
